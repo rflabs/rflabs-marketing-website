07314a61b2f384a1d7b7fbe24c806841600f2106
Kevin Lee <kevi.lee@refreshlabs.co>
2020-03-21 14:22:34 -0700

    new assets

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 import express from "express";
 import bodyParser from "body-parser";
 
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 // port
 const PORT = 8080;
@@ -17,8 +17,12 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(bodyParser.text()); // support encoded bodies
 
+// sendgrid
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 // public at root
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   fs.readFile(path.resolve("public", "index.html"), "utf8", (err, data) => {
     if (err) {
@@ -30,6 +34,22 @@ app.get("/", (req, res) => {
       res.send(data);
     }
   });
+});
+
+app.post("/test-drive-pie", async (req, res) => {
+  try {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: req.body.Email,
+      from: "hello@refreshlabs.com",
+      templateId: "d-d306abf9a0e341f4a0df201e144601cc"
+    };
+    sgMail.send(msg);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 server.listen(PORT, () => {
